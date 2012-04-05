8ace949e125696ddd109dbd4f9e6c95efce8d23b
zengys <ynnuyonw@126.com>
2012-04-05 10:45:58 +0800

    修改空仓号算法，统一所有烟道的空仓号为当前流水号禁止空仓报警 但混合烟道因在手工补货优化之后处理，所以不可再折单，致空仓号 当前订单有可能包含大于尾数7条； Signed-off-by: zengys <ynnuyonw@126.com>

--- Solution_Sorting/THOK.Optimize/HandleSupplyOptimize.cs
+++ Solution_Sorting/THOK.Optimize/HandleSupplyOptimize.cs
@@ -142,7 +142,7 @@ namespace THOK.Optimize
                 channelCount = 0;
                 foreach (DataRow scOrderRow in rows)
                 {
-                    //记录分拣倒数第7条的订单号
+                    //记录分拣倒数第7条的订单号 todo;
                     if (channelCount <= 7 && channelCount + Convert.ToInt32(scOrderRow["QUANTITY"]) >= 7)
                     {
                         channelRow["SORTNO"] = Convert.ToInt32(scOrderRow["SORTNO"]);
